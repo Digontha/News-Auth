18953cf035e8e374b59013ac005e9747421db3c6
Digontha <digonthodj@gmail.com>
2023-10-04 14:30:57 +0600

    add api

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCJVIVZPd1U7kkXm1jMi3mzEo-k3ISI8-M",
-  authDomain: "news-auth-e9948.firebaseapp.com",
-  projectId: "news-auth-e9948",
-  storageBucket: "news-auth-e9948.appspot.com",
-  messagingSenderId: "833415793910",
-  appId: "1:833415793910:web:1dc83280d80689d0f670d7"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
